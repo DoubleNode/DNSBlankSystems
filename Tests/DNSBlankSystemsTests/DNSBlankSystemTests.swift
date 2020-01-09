795041b2b9497a4f5369bc955de6077d019d8f07
Darren Ehlers <me@darrenehlers.com>
2020-01-09 12:55:13 -0600

    Fix typos

--- Tests/DNSBlankSystemsTests/DNSBlankSystemTests.swift
+++ Tests/DNSBlankSystemsTests/DNSBlankSystemTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import DNSCrashWorkers
+@testable import DNSBlankSystems
 
-final class WKRCrashAnalyticsWorkerTests: XCTestCase {
+final class DNSBlankSystemTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
