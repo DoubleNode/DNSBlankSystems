c967357f400319425f6b6362d40d96f10206aa65
Darren Ehlers <me@darrenehlers.com>
2021-02-27 21:17:56 -0600

    Fixed package.swift file

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNSBlankSystems",
-            dependencies: ["DNSProtocols"]),
+            dependencies: ["DNSError", "DNSProtocols"]),
         .testTarget(
             name: "DNSBlankSystemsTests",
             dependencies: ["DNSBlankSystems"]),
